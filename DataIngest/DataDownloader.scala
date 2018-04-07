906a9ce98f0f31a36e38826218e9e5620e001b13
Ankit Bhagat <ankit.bhagat@live.com>
2018-04-06 21:28:13 -0400

    Update DataDownloader.scala

--- DataIngest/DataDownloader.scala
+++ DataIngest/DataDownloader.scala
@@ -8,13 +8,13 @@ def fileDownloader(url: String, filename: String) = {
 
 val date = java.time.LocalDate.now
 
-val 311zipFileName = "311data"+date+".zip"
+val ThreeOneOneZipFileName = "311data"+date+".zip"
 val parkingViolationsFileName = "ParkingViolations"+date+".csv"
 
-fileDownloader("https://data.cityofnewyork.us/download/fpz8-jqf4/application%2Fzip","311data"+date+".zip")
+fileDownloader("https://data.cityofnewyork.us/download/fpz8-jqf4/application%2Fzip",ThreeOneOneZipFileName)
 fileDownloader("https://data.cityofnewyork.us/api/views/2bnn-yakx/rows.csv?accessType=DOWNLOAD", parkingViolationsFileName)
 
-("unzip -o "+311zipFileName).!
+("unzip -o "+ThreeOneOneZipFileName).!
 
 ("hdfs dfs -put 311_Service_Requests_from_2011.csv 311Project/Data/").!
 ("hdfs dfs -put "+parkingViolationsFileName+" 311Project/Data").!
