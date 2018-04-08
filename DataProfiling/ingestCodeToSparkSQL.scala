a4b7b142e7a3a0d79d5f7cd4dc0f7cd49e899ea7
Ankit Bhagat <acb693@nyu.edu>
2018-04-07 20:18:10 -0400

    Adding cleaning and profiling code

--- DataProfiling/ingestCodeToSparkSQL.scala
+++ DataProfiling/ingestCodeToSparkSQL.scala
@@ -6,8 +6,10 @@ import org.apache.spark.sql.SQLContext
 
 val sqlContext = new SQLContext(sc)
 
-val df = sqlContext.read.format("com.databricks.spark.csv")
-		.option("header","true")
-		.load("311Project/Data/311_Service_Requests_from_2011.csv")
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").load("311Project/Data/311_Service_Requests_from_2011.csv")
 
+val dfTwo = df.withColumn("UniqueKey",df("Unique Key").cast(IntegerType))
+val dfThree = dfTwo.withColumn("IncidentZip",df("Incident Zip").cast(IntegerType))
+
+val dfFour = dfThree.filter($"IncidentZip" < 15000)
 
